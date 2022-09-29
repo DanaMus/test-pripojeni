329bd30b2bfda137e6c159e0a26c44081188cd5a
Dana Musilova <dana.musilova@email.cz>
2022-09-29 20:23:25 +0200

    Connection test

--- src/components/Offline/index.jsx
+++ src/components/Offline/index.jsx
@@ -3,15 +3,29 @@ import React, { useEffect, useState } from 'react';
 const Offline = () => {
   const [isOnline, setIsOnline] = useState(navigator.onLine);
 
-  if (isOnline) {
-    return (
-      <p>Všechno v pořádku</p>
-    )
+  const handleOffline = () => {
+    setIsOnline(false);
+    console.log('OfflineHandle');
+  };
+  const handleOnline = () => {
+    setIsOnline(true);
+    console.log('OnlineHandle');
   };
 
-  return (
-    <p>Jste offline!</p>
-  );
+  useEffect(() => {
+    window.addEventListener('offline', handleOffline);
+    window.addEventListener('online', handleOnline);
+    return () => {
+      window.removeEventListener('offline', handleOffline);
+      window.removeEventListener('online', handleOnline);
+    };
+  }, []);
+
+  if (isOnline) {
+    return <p>Všechno v pořádku</p>;
+  }
+
+  return <p>Jste offline!</p>;
 };
 
 export default Offline;
